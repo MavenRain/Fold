ab07e5a95a22dc14c3e333a0eb6831d7ea884f5b
Onyeka Obi <Onyeka.Obi@gmail.com>
2017-05-27 21:57:25 -0700

    Use iterators for fold

--- Fold.h
+++ Fold.h
@@ -1,7 +1,8 @@
-template<unsigned NumberOfItems, typename Accumulator, typename Magma>
-Magma Fold(Magma magma[NumberOfItems])
+template<typename Accumulator, typename BeginIterator, typename EndIterator, typename Range>
+Range Fold(BeginIterator front, EndIterator back, Range initial)
 {
-    Magma sum = magma[0];
-    for (unsigned index = 1; index < NumberOfItems; ++index) sum = Accumulator()(sum, magma[index]);
+    if (front == back) return initial;
+    Range sum = initial;
+    for (BeginIterator index = front; index != back; ++ index) sum = Accumulator()(sum, * index);
     return sum;
 }
