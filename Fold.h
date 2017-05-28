3c8100083bb8a542c2609b1cdce56d0880c5e45b
Onyeka Obi <Onyeka.Obi@gmail.com>
2017-05-27 23:01:51 -0700

    Use class scope to hide iterator implementation details

--- Fold.h
+++ Fold.h
@@ -1,14 +1,16 @@
-template<typename Accumulator, typename BeginIterator, typename EndIterator, typename Range>
-Range FoldImpl(BeginIterator front, EndIterator back, Range initial)
-{
-    if (front == back) return initial;
-    Range sum = initial;
-    for (BeginIterator index = front; index != back; ++ index) sum = Accumulator()(sum, * index);
-    return sum;
-}
 
-template<typename Action, typename Container, typename Range>
-Range Fold(Container container, Range initial)
-{
-    return FoldImpl<Action>(container.begin(), container.end(), initial);
-}
+    template<typename Accumulator, typename BeginIterator, typename EndIterator, typename Range>
+    static Range FoldImpl(BeginIterator front, EndIterator back, Range initial)
+    {
+        if (front == back) return initial;
+        Range sum = initial;
+        for (BeginIterator index = front; index != back; ++ index) sum = Accumulator()(sum, * index);
+        return sum;
+    }
+public:
+    template<typename Action, typename Container, typename Range>
+    static Range Fold(Container container, Range initial)
+    {
+        return FoldImpl<Action>(container.begin(), container.end(), initial);
+    }
+};
