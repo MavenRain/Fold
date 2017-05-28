ae3c88e0b60cb5cbe27c0881ed5c499a47c90e6c
Onyeka Obi <Onyeka.Obi@gmail.com>
2017-05-27 22:32:14 -0700

    Abstract iterator details

--- Fold.h
+++ Fold.h
@@ -1,8 +1,14 @@
 template<typename Accumulator, typename BeginIterator, typename EndIterator, typename Range>
-Range Fold(BeginIterator front, EndIterator back, Range initial)
+Range FoldImpl(BeginIterator front, EndIterator back, Range initial)
 {
     if (front == back) return initial;
     Range sum = initial;
     for (BeginIterator index = front; index != back; ++ index) sum = Accumulator()(sum, * index);
     return sum;
+}
+
+template<typename Action, typename Container, typename Range>
+Range Fold(Container container, Range initial)
+{
+    return FoldImpl<Action>(container.begin(), container.end(), initial);
 }
