530f955e631ce4c1b5001afb49628694a1eed74a
Onyeka Obi <Onyeka.Obi@gmail.com>
2017-05-28 11:15:45 -0700

    Need to allow iterator implementation to be visible for C-style arrays

--- Fold.h
+++ Fold.h
@@ -1,5 +1,5 @@
 template<typename Accumulator, typename BeginIterator, typename EndIterator, typename Range>
-static Range Fold(BeginIterator front, EndIterator back, Range initial)
+Range Fold(BeginIterator front, EndIterator back, Range initial)
 {
     if (front == back) return initial;
     Range sum = initial;
@@ -7,7 +7,7 @@ static Range Fold(BeginIterator front, EndIterator back, Range initial)
     return sum;
 }
 template<typename Action, typename Container, typename Range>
-static Range Fold(Container container, Range initial)
+Range Fold(Container container, Range initial)
 {
     return Fold<Action>(container.begin(), container.end(), initial);
 }
